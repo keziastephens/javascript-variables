829f055fec3ecb4a034be00352bbf4e14d69f133
keziastephens <87955878+keziastephens@users.noreply.github.com>
2021-10-07 16:46:51 +1300

    Add files via upload

---
+++ js practice/script.js
@@ -0,0 +1,104 @@
+console.log("wasss gooood");
+
+console.log ( Date() ); 
+
+// variable is a symbolic name for a value
+// declare our variable with a key word
+
+let number = 0;
+console.log(number);
+
+
+let name = "kezia";
+console.log(name);
+
+let kezcoffee = 3;
+let liamcoffee = 1;
+let ajcoffee = 2;
+
+
+// numbers
+// + adds numbers together
+// - subtracts numbers
+// * times numbers 
+// / divides numbers
+
+
+
+// console.log(kezcoffee + liamcoffee)
+
+let totalcoffee = kezcoffee + liamcoffee + ajcoffee;
+console.log(totalcoffee);
+
+// var number = 0;
+
+// alert("can i have " + kezcoffee + " coffees");
+
+let pasta;
+console.log(pasta);
+
+pasta = "spaghetti";
+
+let people;
+
+let adult = 1;
+let child = 1;
+
+console.log("you are buying " + adult + " adult tickets");
+console.log("you are buying " + child + " child tickets");
+
+people = adult + child;
+console.log("you are buying tickets for " + people + " people");
+
+let adultTicket = 50;
+let childTicket = 25;
+
+console.log("a child ticket cost is $" +childTicket)
+console.log("unluggy uce")
+
+let totalTicket = adultTicket + childTicket;
+console.log(totalTicket)
+
+// the task is to create an email account and display in the console
+// the email address 
+// primary inbox displaying total amount of emails
+// spam inbox displaying total amount of emails
+// total amount of emails  of both inboxes
+
+let user = "keziastephens@gmail.com"
+console.log("this is the inbox for the email account for "+user)
+
+
+let primaryDeleted = 10;
+let primaryEmail = 500 - primaryDeleted;
+console.log ("you have " +primaryEmail+ " emails in your primary inbox")
+
+
+let spamEmail = 14;
+console.log("you have " + spamEmail + " emails in your spam inbox")
+
+let totalInbox = primaryEmail + spamEmail;
+console.log("your total amount of emails are " +totalInbox)
+
+
+
+// const is a type of variable its the same as a variable in the respect that it stores data
+// the difference is that a const needs to consistanly have a defined value assigned. Where as let does not need to have an assigned value
+
+
+// let festival;
+// console.log(festival)
+
+const festival = spamEmail;
+console.log(festival);
+
+let movieTicket;
+let movie_ticket;
+let _movieTicket;
+let _movie;
+
+let example = "today jake taught us about variables and he said that \"a variable holds data\" ";
+console.log(example);
+let exampleTwo = `our spam email inbox has ${spamEmail} emails`
+console.log(exampleTwo)
+
